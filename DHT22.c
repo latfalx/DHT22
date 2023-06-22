e015a350bc02e7d614d39e9afabbe0163f806c4b
latfalx <54545929+latfalx@users.noreply.github.com>
2023-06-22 21:46:13 +0500

    Update DHT22.c

--- DHT22.c
+++ DHT22.c
@@ -25,7 +25,7 @@ unsigned short DHT22_AskReady(){
 	} else			
 	return 1;
 }
-//--------------------------------/	Считывания байта	
+//--------------------------------/	Считывания байта
 unsigned char DHT22_ReadByte(){
 	int i, run, step=0;
 	unsigned char rbyte=0;
@@ -44,7 +44,7 @@ unsigned char DHT22_ReadByte(){
 					break;	
 				case DHT22_LOW:
 					if(step){
-						if(step<11) rbyte &= ~(1<<(7-i));	// Предел значений 7..16
+						if(step<11) rbyte &= ~(1<<(7-i));	// Предел step<[7..16]
 						else rbyte |= 1<<(7-i);
 					step = 0;
 					run = 0;
@@ -56,36 +56,9 @@ unsigned char DHT22_ReadByte(){
 	}
 	return rbyte;
 }
-//--------------------------------/	Не используется (рабочий вариант)
-unsigned char DHT22_ReadByte2(){
-	int i,sk;
-	unsigned char rbyte=0;
-	for(i=0; i<8; i++){
-		sk=0;
-		while(~DHT22_STATUS){
-			sk++;
-			if (sk>100) break;
-			_delay_us(1);
-		}
-		_delay_us(30);
-		if(~DHT22_STATUS) rbyte &= ~(1<<(7-i));
-		else {
-			rbyte |= 1<<(7-i);
-			while(DHT22_STATUS){
-				sk++;
-				if(sk>100) break;
-				_delay_us(1);
-			}
-		}
-	}
-	return rbyte;
-}
 //--------------------------------/	Склеиваем байты в слово
 unsigned short DHT22_GlueByte(unsigned char byte1, unsigned char byte2){
-	unsigned short int res;
-	res = byte1;
-	res = (res<<8) | byte2;
-	return res;
+	return (byte1<<8) | byte2;
 }
 //--------------------------------/	Проверка подлинности слов полученных от датчика
 unsigned short DHT22_GetWord(unsigned short *word1, unsigned short *word2){
@@ -125,12 +98,12 @@ unsigned short DHT22_AskStrSensor(char *shmr, char *stmr){
 			if(!(hmr/100)) shmr[0]=20;	// Если нет десятичных
 			else shmr[0]=(hmr/100)+48;	// Десятичные	
 			shmr[1]=((hmr/10)%10)+48;	// Единицы
-			shmr[2]=0;	// Запятая 0x2C
-			shmr[3]=0;	// Дробная часть (hmr%10)+48
+			shmr[2]=0;			// Запятая 0x2C
+			shmr[3]=0;			// Дробная часть (hmr%10)+48
 			shmr[4]=0;								
 		}
 		// Температура										
-		if(tmr==0) stmr[0]=20;	// 00,0 Убераем первый 0
+		if(tmr==0) stmr[0]=20;		// 00,0 Убераем первый 0
 		else 
 			if(tmr>0x8000){		// Определяем знак
 				tmr &= 0x7FFF;	// Убераем бит знака
@@ -151,5 +124,3 @@ unsigned short DHT22_AskStrSensor(char *shmr, char *stmr){
 	}
 	return 0;
 }
-
-
